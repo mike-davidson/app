1bb7f9ef2165c33a4372193c8ab5889d7da37981
git_name <git_email>
2012-12-06 13:53:50 -0500

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,29 @@
-﻿namespace app.web
+﻿using System.Collections.Generic;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
+      private IEnumerable<IRunOneRequest> all_commands;
+
+      public CommandRegistry(IEnumerable<IRunOneRequest> all_commands)
+      {
+          this.all_commands = all_commands;
+      }
+
     public IRunOneRequest get_the_command_that_can_run(IContainRequestDetails the_request)
     {
-      throw new System.NotImplementedException();
+        IRunOneRequest requestToReturn = new NotFoundRequest(); 
+
+        foreach (var runOneRequest in all_commands)
+        {
+            if (runOneRequest.can_run(the_request))
+            {
+                requestToReturn = runOneRequest;
+            }
+        }
+
+        return requestToReturn;
     }
   }
 }
---
+++ source/app/web/NotFoundRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace app.web
+{
+    public class NotFoundRequest : IRunOneRequest
+    {
+        public void process(IContainRequestDetails the_request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool can_run(IContainRequestDetails the_request)
+        {
+            return false;
+        }
+    }
+}
