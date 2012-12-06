fbc7e9d7a89bcfa0c12740f3991e53bc70d353d5
git_name <git_email>
2012-12-06 14:08:58 -0500

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -6,15 +6,17 @@ namespace app.web
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IRunOneRequest> all_commands;
+      private IHandleTheScenarioOfCreatingTheMissingCommand special_case_creator;
 
-    public CommandRegistry(IEnumerable<IRunOneRequest> all_commands)
+    public CommandRegistry(IEnumerable<IRunOneRequest> all_commands, IHandleTheScenarioOfCreatingTheMissingCommand special_case_creator)
     {
       this.all_commands = all_commands;
+        this.special_case_creator = special_case_creator;
     }
 
     public IRunOneRequest get_the_command_that_can_run(IContainRequestDetails the_request)
     {
-      return all_commands.First(x => x.can_run(the_request));
+      return all_commands.FirstOrDefault(x => x.can_run(the_request)) ?? special_case_creator();
     }
   }
 }
