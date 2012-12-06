43c2be370e28376c9edea2d616cda1bd3485a52c
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-12-06 11:57:59 -0700

    Updated

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -4,26 +4,21 @@ namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
-      private IEnumerable<IRunOneRequest> all_commands;
+    IEnumerable<IRunOneRequest> all_commands;
 
-      public CommandRegistry(IEnumerable<IRunOneRequest> all_commands)
-      {
-          this.all_commands = all_commands;
-      }
+    public CommandRegistry(IEnumerable<IRunOneRequest> all_commands)
+    {
+      this.all_commands = all_commands;
+    }
 
     public IRunOneRequest get_the_command_that_can_run(IContainRequestDetails the_request)
     {
-        IRunOneRequest requestToReturn = new NotFoundRequest(); 
+      foreach (var runOneRequest in all_commands)
+      {
+        if (runOneRequest.can_run(the_request)) return  runOneRequest;
+      }
 
-        foreach (var runOneRequest in all_commands)
-        {
-            if (runOneRequest.can_run(the_request))
-            {
-                requestToReturn = runOneRequest;
-            }
-        }
-
-        return requestToReturn;
+      return null;
     }
   }
 }
