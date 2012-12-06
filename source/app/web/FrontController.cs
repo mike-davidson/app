07212bbaff7027b4febf7591f0826a57bf2bcd2b
ljblackburn <lblackburn@deltadentalmi.com>
2012-12-06 13:24:26 -0500

    Pushing new changes

--- source/app/web/FrontController.cs
+++ source/app/web/FrontController.cs
@@ -2,9 +2,17 @@
 {
   public class FrontController : IProcessRequests
   {
+    IFindCommands findCommands;
+
+    public FrontController(IFindCommands findCommands)
+    {
+       this.findCommands = findCommands;
+    }
+
     public void process(object the_request)
     {
-      throw new System.NotImplementedException();
+      var command = findCommands.get_the_command_that_can_run(the_request);
+      command.process(the_request);
     }
   }
 }
